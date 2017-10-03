2c1ed8139e4baa395b0319ff743cde986f944c52
steinnp <steiniell@gmail.com>
2017-10-03 22:01:02 +0200

    preprocessor setup

---
+++ TextCleaning/preprocessor.py
@@ -0,0 +1,33 @@
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
+import random
+stemmer = LancasterStemmer()
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from pprint import pprint
+
+class Preprocessor:
+    def __parse_review(self, reviewString):
+        letters_only = re.sub('[^a-zA-Z]', ' ', reviewString) # replace non-letters
+        letters_only = word_tokenize(letters_only)
+        letters_only = " ".join(letters_only)
+        lower_case = letters_only.lower()        # Convert to lower case
+        words = lower_case.split()               # Split into words
+        words = [stemmer.stem(w) for w in words if not w in stopwords.words("english")]
+        return " ".join(words)
+
+    def __init__(self, content):
+        self.__vectorizer = TfidfVectorizer(stop_words = stopwords.words('english'), preprocessor = self.__parse_review)
+        self.__transformedValues = self.__vectorizer.fit_transform(content)
+
+    def get_vocabulary(self):
+        return self.__vectorizer.vocabulary_
+
+    def get_transformed_values(self):
+        return self.__transformedValues
+
+    def transform(self, content):
+        return self.__vectorizer.transform(content)
